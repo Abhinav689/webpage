9799da98e92ed86000fedc709d02ef07661bd761
Abhinav689 <abhinavsai689@gmail.com>
2023-07-19 22:44:25 +0530

    Updates

---
+++ precache-manifest.5757e6a11550aa194d2550135f8a8f89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e17412233dfce6515f4ba07b4faa7a6",
+    "url": "/webpage/index.html"
+  },
+  {
+    "revision": "4386dcfc90ab99dec95e",
+    "url": "/webpage/static/css/main.f620f5e6.chunk.css"
+  },
+  {
+    "revision": "1d9347a719a5d18f2383",
+    "url": "/webpage/static/js/2.370251b4.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/webpage/static/js/2.370251b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4386dcfc90ab99dec95e",
+    "url": "/webpage/static/js/main.12fc6cc3.chunk.js"
+  },
+  {
+    "revision": "c5d4a684d01792e64d5d",
+    "url": "/webpage/static/js/runtime-main.69ab95e4.js"
+  },
+  {
+    "revision": "ae065ea48e1fd3b889686e0c2944b080",
+    "url": "/webpage/static/media/img-1.ae065ea4.jpg"
+  },
+  {
+    "revision": "863870c28663481746a69e3d0fb1e797",
+    "url": "/webpage/static/media/img-5.863870c2.png"
+  }
+]);
